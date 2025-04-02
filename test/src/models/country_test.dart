3556f4b91f91c409101346188e6a95afb7a18d47
fulleni <fulleni@hotmail.com>
2025-04-02 19:03:16 +0100

    test: update country model tests
    - Added id to country model tests
    - Fixed equality tests
    - Added id to json serialization tests

--- test/src/models/country_test.dart
+++ test/src/models/country_test.dart
@@ -1,20 +1,30 @@
+// ignore_for_file: lines_longer_than_80_chars
+
 import 'package:ht_countries_client/ht_countries_client.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:test/test.dart';
 
+import 'package:uuid/uuid.dart';
+
 void main() {
   group('Country Model', () {
+    final id1 = const Uuid().v4();
+    final id2 = const Uuid().v4();
+
     const isoCode = 'DZ';
     const name = 'Algeria';
     const flagUrl = 'https://flagcdn.com/dz.svg';
-    const country = Country(isoCode: isoCode, name: name, flagUrl: flagUrl);
+    final country =
+        Country(id: id1, isoCode: isoCode, name: name, flagUrl: flagUrl);
 
     const isoCode2 = 'US';
     const name2 = 'United States';
     const flagUrl2 = 'https://flagcdn.com/us.svg';
-    const country2 = Country(isoCode: isoCode2, name: name2, flagUrl: flagUrl2);
+    final country2 =
+        Country(id: id2, isoCode: isoCode2, name: name2, flagUrl: flagUrl2);
 
     final countryJson = {
+      'id': id1,
       'iso_code': isoCode,
       'name': name,
       'flag_url': flagUrl,
@@ -22,6 +32,7 @@ void main() {
 
     test('can be instantiated', () {
       expect(country, isNotNull);
+      expect(country.id, id1);
       expect(country.isoCode, isoCode);
       expect(country.name, name);
       expect(country.flagUrl, flagUrl);
@@ -30,13 +41,13 @@ void main() {
     group('Equatable', () {
       test('supports value equality', () {
         expect(
-          const Country(isoCode: isoCode, name: name, flagUrl: flagUrl),
+          Country(id: id1, isoCode: isoCode, name: name, flagUrl: flagUrl),
           equals(country),
         );
       });
 
       test('props are correct', () {
-        expect(country.props, equals([isoCode, name, flagUrl]));
+        expect(country.props, equals([id1, isoCode, name, flagUrl]));
       });
 
       test('instances with different props are not equal', () {
@@ -46,22 +57,23 @@ void main() {
 
     group('Serialization', () {
       test('fromJson creates correct object', () {
-         expect(Country.fromJson(countryJson), equals(country));
-       });
+        expect(Country.fromJson(countryJson), equals(country));
+      });
 
-       test('fromJson throws CheckedFromJsonException for invalid JSON', () {
-         // Provide required keys but with incorrect types (e.g., int for String)
-         // to trigger CheckedFromJsonException due to type mismatch during parsing.
-         final invalidJson = {
-           'iso_code': 123, // Invalid type (int) for required String
-           'name': 'Invalid Name',
-           'flag_url': 'http://example.com/flag.png', // Valid string here
-         };
-         expect(
-           () => Country.fromJson(invalidJson),
-           throwsA(isA<CheckedFromJsonException>()),
-         );
-       });
+      test('fromJson throws CheckedFromJsonException for invalid JSON', () {
+        // Provide required keys but with incorrect types (e.g., int for String)
+        // to trigger CheckedFromJsonException due to type mismatch during parsing.
+        final invalidJson = {
+          'id': const Uuid().v4(),
+          'iso_code': 123, // Invalid type (int) for required String
+          'name': 'Invalid Name',
+          'flag_url': 'http://example.com/flag.png', // Valid string here
+        };
+        expect(
+          () => Country.fromJson(invalidJson),
+          throwsA(isA<CheckedFromJsonException>()),
+        );
+      });
 
       test('toJson returns correct map', () {
         expect(country.toJson(), equals(countryJson));
