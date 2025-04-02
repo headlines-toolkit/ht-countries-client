18c7fede11091c04bf773ea2a4bda0023e4fc2ba
fulleni <fulleni@hotmail.com>
2025-04-02 18:21:41 +0100

    chore: initial commit
    - Added basic project structure
    - Created initial client class
    - Added issue and PR templates

---
+++ lib/src/ht_countries_client.dart
@@ -0,0 +1,7 @@
+/// {@template ht_countries_client}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtCountriesClient {
+  /// {@macro ht_countries_client}
+  const HtCountriesClient();
+}
---
+++ test/src/ht_countries_client_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_countries_client/ht_countries_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtCountriesClient', () {
+    test('can be instantiated', () {
+      expect(HtCountriesClient(), isNotNull);
+    });
+  });
+}
